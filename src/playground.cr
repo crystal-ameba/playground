f4dc71ce6c4da671c9f0127e49bbf53d2b83e6d3
Vitalii Elenhaupt <velenhaupt@gmail.com>
2019-12-21 17:38:03 +0200

    New test

--- src/playground.cr
+++ src/playground.cr
@@ -1,6 +1,7 @@
 # TODO: Write documentation for `Playground`
 module Playground
   VERSION = "0.1.0"
+  POOL = [] of String
 
   # TODO: Put your code here
   def test
@@ -9,5 +10,10 @@ module Playground
 
   def test2
     a = 2
+  end
+
+  def test3
+    value, score = POOL.redis { |c| c.zrange("retry", 0, -1, with_scores: true) }.as(Array)
+    puts value
   end
 end
